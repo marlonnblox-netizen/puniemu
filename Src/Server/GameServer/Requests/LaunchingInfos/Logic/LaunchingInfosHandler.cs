28631a5f5d1ae092e513b293d776a0ddbd198c52
marlonnblox-netizen <marlonnblox@gmail.com>
2026-09-11 04:15:34 +0200

    Update LaunchingInfosHandler.cs

--- Src/Server/GameServer/Requests/LaunchingInfos/Logic/LaunchingInfosHandler.cs
+++ Src/Server/GameServer/Requests/LaunchingInfos/Logic/LaunchingInfosHandler.cs
@@ -17,16 +17,91 @@ namespace Puniemu.Src.Server.GameServer.Requests.LaunchingInfos.Logic
                 loginable = "Y",
                 playable = "Y",
                 currentTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                heartBeatInterval = 60,
                 cacheTTLSec = 3600,
-                loginUrl = $"{baseUrl}/login.nhn",
+                brandType = "TOASTGAME",
+                serviceDomain = "GLOBAL",
+
                 serverInfos = new Dictionary<string, string>
                 {
+                    { "LOGIN", baseUrl },
+                    { "GAMESVR", baseUrl },
+                    { "CS", baseUrl },
+                    { "NOTICE", baseUrl },
+                    { "IMAGE", baseUrl },
+                    { "IMAGE_RESIZE", baseUrl },
+                    { "MFS", baseUrl },
+                    { "LCS", baseUrl },
+                    { "NOMAD", baseUrl },
+                    { "PHOTO", baseUrl },
+                    { "SILOS", baseUrl },
+                    { "GAME_PLUS", baseUrl },
+                    { "GAME_INDICATOR_BIP", baseUrl },
+                    { "RTA_BIP", baseUrl },
+                    { "STABILITY_BIP", baseUrl },
                     { "GAME_SERVER", baseUrl }
                 },
-                serviceDomain = "GLOBAL",
-                serviceInfo = new { },
+
+                loginUrl = new
+                {
+                    hangameLoginUrl_JP = baseUrl,
+                    hangamejp = baseUrl,
+                    hangameOAuthUrl = $"{baseUrl}/authorize.nhn",
+                    hangameEmailOAuthUrl = $"{baseUrl}/egm/login.nhn"
+                },
+
+                userAuthentication = new
+                {
+                    checkAuthUrl = $"{baseUrl}/hsp/checkAuth.nhn",
+                    reAuthUrl = $"{baseUrl}/hsp/reAuth.nhn"
+                },
+
+                idpInfo = new
+                {
+                    hangame = new { selected = "Y", loginable = "Y", consumerKey = "DUMMY_KEY", consumerSecret = "DUMMY_SECRET", redirectionUrl = baseUrl },
+                    hangamejp = new { selected = "Y", loginable = "Y", consumerKey = "DUMMY_KEY", consumerSecret = "DUMMY_SECRET", redirectionUrl = baseUrl },
+                    google = new { selected = "N", loginable = "N", consumerKey = "", consumerSecret = "", redirectionUrl = "" },
+                    facebook = new { selected = "N", loginable = "N", consumerKey = "", consumerSecret = "", redirectionUrl = "" },
+                    gree = new { selected = "N", loginable = "N", consumerKey = "", consumerSecret = "", redirectionUrl = "" }
+                },
+
+                lncNotices = new object[] { },
+
+                serviceInfo = new { maxRankingSize = 100 },
+
+                clientAttributes = new
+                {
+                    ENFORCED_IDP_LOGIN = "N",
+                    HOLD_IDP_LOGIN = "N"
+                },
+
+                payment = new { selected = "N" },
+
+                logncrash = new
+                {
+                    appKey = "DUMMY_APPKEY",
+                    logLevel = "ERROR",
+                    logtype = "NELO2"
+                },
+
+                stabilityStatus = "Y",
+
+                toastCloudAppInfo = new
+                {
+                    Push = new { appKey = "DUMMY_PUSH_APPKEY" }
+                },
+
+                indicatorLogServer = baseUrl,
+
+                loginUrlSimple = $"{baseUrl}/login.nhn",
                 termsUrl = $"{baseUrl}/help/inquiry/top.nhn",
-                csUrl = $"{baseUrl}/help/inquiry/top.nhn"
+                csUrl = $"{baseUrl}/help/inquiry/top.nhn",
+                personalInfoCollectionUrl = $"{baseUrl}/help/inquiry/top.nhn",
+                punishReasonUrl = $"{baseUrl}/help/inquiry/top.nhn",
+
+                LGCConfigKey = "",
+                LGCGameUrl = "",
+                LGCGcmProjectNum = ""
             };
 
             var json = JsonConvert.SerializeObject(response);
